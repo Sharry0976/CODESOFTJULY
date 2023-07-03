fe74b8519b996ab778de5835cbe35025a8f6028a
Shahroz Khan <shahrozkhan0976@gmail.com>
2023-07-03 17:06:55 +0530

    core functionality part 1
    Core functionality part 1 has been done where the input text module has been done, now we only need to add filesystem processing to get the  job done.

--- Tasks/Task_2/WordCounter_Main_Logic.java
+++ Tasks/Task_2/WordCounter_Main_Logic.java
@@ -1,2 +1,12 @@
+//import java.util.ArrayList;
+
 public class WordCounter_Main_Logic {
+    public static int normalWordSize(String s)
+    {
+        s.trim();
+        String words[]=s.split("[, .  ]+");
+        int size=words.length;
+        
+        return size;
+    }
 }
--- Tasks/Task_2/Word_Counter.java
+++ Tasks/Task_2/Word_Counter.java
@@ -1,13 +1,15 @@
 import java.util.*;
-public class Word_Counter
+public class Word_Counter extends WordCounter_Main_Logic
 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Word Counter:");
         System.out.println("Text File(F) or You want to write(T)?");
         System.out.print("Specify(T or F):");//here T specifies 
-        char ch=sc.next().charAt(0);
-        if(Character.isAlphabetic(ch)&&(ch=='t'||ch=='T')&&(ch=='f'||ch=='F'))
+        char ch=sc.nextLine().charAt(0);
+        
+        int word_size=0;
+        if(Character.isLetter(ch)&&(ch=='t'||ch=='T'||ch=='f'||ch=='F'))
             ch=Character.toUpperCase(ch);
         else
         {
@@ -15,12 +17,15 @@ public class Word_Counter
         }
         if(ch=='T')
         {
-
+            System.out.println("Enter the Text:");
+            String str=sc.nextLine();
+            word_size=normalWordSize(str);
         }
         else if(ch=='F')
         {
 
         }
+        System.out.println("Word size:"+word_size);
         sc.close();
     }
 }
