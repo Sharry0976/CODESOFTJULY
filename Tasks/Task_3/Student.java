f24b36fd0c5f598e8e10223c39f08a11808dc339
Shahroz Khan <shahrozkhan0976@gmail.com>
2023-07-22 22:35:47 +0530

    3rd Task Completed.

--- Tasks/Task_3/Student.java
+++ Tasks/Task_3/Student.java
@@ -1,6 +1,124 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Student
-{
-    public static void main(String[] args) {
-        
+    {
+    public static void main(String[] args)throws IOException{
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("------Student Management System------");
+        System.out.println("Options------");
+        System.out.println("Press 1 to add student.");
+        System.out.println("Press 2 to edit Existing student's information.");
+        System.out.println("Press 3 to search for a student.");
+        System.out.println("Press 4 to display all student.");
+        System.out.println("Press 5 to delete students.");
+        System.out.println("Press 0 to exit.");
+        int ch;
+        int reg_Id;//registration no
+        String name;
+        String dateOfBirth;
+        int Class;
+        int roll;
+        String s;
+
+        StudentMgmtSystem student=new StudentMgmtSystem();
+    
+        do
+        {
+            System.out.print("Enter a choice:");
+            ch=sc.nextInt();
+
+            switch (ch) {
+                case 1://Add Student
+                    System.out.println("Enter Registration id:");
+                    reg_Id=sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Enter Name:");
+                    name=sc.nextLine();
+                    System.out.println("Enter Date of Birth:");
+                    dateOfBirth=sc.next();
+                    System.out.println("Enter Class:");
+                    Class=sc.nextInt();
+                    System.out.println("Enter roll no:");
+                    roll=sc.nextInt();
+
+                    student.addStudent(reg_Id,name,dateOfBirth,Class,roll);
+                    break;
+                case 2://Edit student
+                    System.out.println("Enter Registration id:");
+                    reg_Id=sc.nextInt();
+                    sc.nextLine();
+                    s=student.searchStudent(reg_Id);
+                    if(s!="")
+                    {
+                        System.out.println("Enter Name:");
+                        name=sc.nextLine();
+                        System.out.println("Enter Date of Birth:");
+                        dateOfBirth=sc.next();
+                        System.out.println("Enter Class:");
+                        Class=sc.nextInt();
+                        System.out.println("Enter roll no:");
+                        roll=sc.nextInt();
+                        String updatedString=reg_Id+" "+name+" "+dateOfBirth+" "+Class+" "+roll+" ";
+                        student.editExistingStudentData(reg_Id,updatedString);
+                    }
+                    else
+                    {
+                        System.out.println("Record Not Present, just add it in StudentData");
+                    }
+
+                    break;
+                case 3://Search for student
+                    System.out.println("Enter the registration id");
+                    reg_Id=sc.nextInt();
+                    s=student.searchStudent(reg_Id);
+                    if(s=="")
+                    {
+                        System.out.println("Record not present:");
+                    }
+                    else
+                    {
+                        String[] st=s.split(" ");
+                        System.out.println("ID\t Name \t Date of Birth \t Class \t Roll ");
+                        for(int i=0;i<st.length;i++)
+                        {
+                            System.out.print(st[i]+"\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 4://display all Students
+                    student.displayAll();
+                    break;
+                case 5://delete student
+                    System.out.print("Delete all(A) or just 1 student(S):");
+                    char chr=sc.next().charAt(0);
+                    chr=Character.toUpperCase(chr);
+                    if(chr=='A')
+                        student.DeleteAllStudent();
+                    else if(chr=='S')
+                    {
+                        System.out.print("Enter Student id:");
+                        reg_Id=sc.nextInt();
+                        student.DeleteStudent(reg_Id);
+                    }
+                    break;
+                case 0:
+                    System.out.println("Exiting Program!!!");
+                    System.exit(0);
+                    break;
+            
+                default:
+                    break;
+            }
+        }while (ch!=0);
+
+
+
+
+        sc.close();
     }
+
+
 }
--- Tasks/Task_3/StudentMgmtSystem.java
+++ Tasks/Task_3/StudentMgmtSystem.java
@@ -1,3 +1,146 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 public class StudentMgmtSystem {
-    
+    public void addStudent(int reg_Id,String name,String dateOfBirth,int Class,int roll)throws IOException
+    {  
+        PrintWriter outFile=new PrintWriter(new FileWriter("StudentRecords.txt",true));
+        outFile.print(reg_Id+" ");
+        outFile.print(name+" ");
+        outFile.print(dateOfBirth+" ");
+        outFile.print(Class+" ");
+        outFile.println(roll+" ");
+        outFile.close();
+    }
+
+    public void editExistingStudentData(int reg_Id,String updatedString)throws FileNotFoundException,IOException
+    {
+        File file=new File("StudentRecords.txt");
+        List<String> lines=new ArrayList<>();
+        Scanner sc=new Scanner(file);
+
+        while(sc.hasNextLine())
+        {
+            lines.add(sc.nextLine());
+        }
+        sc.close();
+
+        boolean recordFound=false;
+        for(int i=0;i<lines.size();i++)
+        {
+            String line=lines.get(i);
+            String[] st=line.split(" ");
+
+            int studentId=Integer.parseInt(st[0].trim());
+
+            if(studentId==reg_Id)
+            {
+                lines.set(i,updatedString);
+                recordFound=true;
+                break;
+            }
+        }
+        if(recordFound)
+        {
+            BufferedWriter writer=new BufferedWriter(new FileWriter(file));
+            for(String line:lines)
+            {
+                writer.write(line);
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("Record Updated");
+        }
+    }
+
+    public String searchStudent(int reg_Id)throws IOException,NumberFormatException
+    {
+        String s="";
+        File file=new File("StudentRecords.txt");
+        Scanner sc=new Scanner(file);
+        //boolean recordFound=false;
+        while(sc.hasNextLine())
+        {
+            String line=sc.nextLine();
+            String[] st=line.split(" ");
+
+            int stId=Integer.parseInt(st[0].trim());
+            if(stId==reg_Id)
+            {
+                s=line;
+                //recordFound=true;
+            }
+        }
+        sc.close();
+        return s;
+
+    }
+    public void displayAll()throws IOException
+    {
+        System.out.println("ID\tName \tDate of Birth \tClass \tRoll ");
+        Scanner inFile=new Scanner(new FileReader("StudentRecords.txt"));
+        while(inFile.hasNextLine())
+        {  
+            String s=inFile.nextLine();
+            String[] st=s.split(" ");
+            for(int i=0;i<st.length;i++)
+            {
+                System.out.print(st[i]+"\t");
+            }
+            System.out.println();
+        }
+    }
+    public void DeleteAllStudent()throws IOException
+    {
+        new FileWriter("StudentRecords.txt", false).close();
+        System.out.println("Deletion Successful");
+    }
+    public void DeleteStudent(int reg_Id)throws IOException
+    {
+        File file=new File("StudentRecords.txt");
+        List<String> lines=new ArrayList<>();
+
+        Scanner sc=new Scanner(file);
+        while(sc.hasNextLine())
+        {
+            lines.add(sc.nextLine());
+        }
+        sc.close();
+        boolean recordFound=false;
+        for(Iterator<String> iterator=lines.iterator();iterator.hasNext();){
+            String line=iterator.next();
+            String[] stRec=line.split(" ");
+            int stId=Integer.parseInt(stRec[0].trim());
+            if(stId==reg_Id)
+            {
+                iterator.remove();
+                recordFound=true;
+                break;
+            }
+        }
+
+        if(recordFound){
+            BufferedWriter writer=new BufferedWriter(new FileWriter(file));
+            for(String line:lines)
+            {
+                writer.write(line);
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("Record deleted Successfully");
+        }
+        else
+        {
+            System.out.println("Record does not exist");
+        }
+    }
 }
